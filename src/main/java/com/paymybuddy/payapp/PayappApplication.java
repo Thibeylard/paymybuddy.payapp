211436375527adf17a8338c8e35d80f1d40f4d12
Thibeylard <thibaut.beylard@lilo.org>
2020-05-04 15:28:52 +0200

    Project initialization with start.spring

---
+++ src/main/java/com/paymybuddy/payapp/PayappApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.payapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayappApplication.class, args);
+	}
+
+}

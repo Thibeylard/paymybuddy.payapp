e02d7343c55125c1dbdea0f4e210a43faa6e8313
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2020-05-14 12:16:33 +0200

    Progress on jdbc authentication

--- src/main/java/com/paymybuddy/payapp/config/SecurityConfiguration.java
+++ src/main/java/com/paymybuddy/payapp/config/SecurityConfiguration.java
@@ -1,15 +1,37 @@
 package com.paymybuddy.payapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource datasource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(datasource);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/home").authenticated()
+                .antMatchers("/login").permitAll()
+                .and().formLogin();
+    }
 
     @Profile("dev")
     @Override

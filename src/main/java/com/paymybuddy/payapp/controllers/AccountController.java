955b6669e16bc5707eb8e2613bdbf43a84e87532
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2020-05-16 12:06:15 +0200

    Create first test class for accountController

--- src/main/java/com/paymybuddy/payapp/controllers/AccountController.java
+++ src/main/java/com/paymybuddy/payapp/controllers/AccountController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(produces = "application/json")
-public class LoginController {
+public class AccountController {
 
     // TODO définir type retour
     /*
@@ -17,6 +17,12 @@ public class LoginController {
     // TODO définir type retour
     @GetMapping("/logout")
     public void logout() {
+
+    }
+
+    // TODO définir type retour
+    @PostMapping("/registration")
+    public void registration() {
 
     }
     */

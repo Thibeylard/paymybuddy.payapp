7d42457be44c816b664f93fe12e1ece69a47dcfd
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2020-05-23 11:41:04 +0200

    RememberMe persistent token configuration

--- src/main/java/com/paymybuddy/payapp/config/AppConfig.java
+++ src/main/java/com/paymybuddy/payapp/config/AppConfig.java
@@ -1,8 +1,15 @@
 package com.paymybuddy.payapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +17,18 @@ public class AppConfig {
     @Bean
     public MethodValidationPostProcessor validationPostProcessor() {
         return new MethodValidationPostProcessor();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Autowired
+    public PersistentTokenRepository persistentTokenRepository(final DataSource dataSource) {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
     }
 }
--- src/main/java/com/paymybuddy/payapp/config/SecurityConfiguration.java
+++ src/main/java/com/paymybuddy/payapp/config/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.paymybuddy.payapp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,20 +10,28 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder bcryptEncoder;
+    private final PersistentTokenRepository jdbcTokenRepository;
+
     @Autowired
-    @Qualifier("userCredentialsService")
-    UserDetailsService userDetailsService;
+    public SecurityConfiguration(@Qualifier("userCredentialsService") UserDetailsService userDetailsService,
+                                 PasswordEncoder passwordEncoder,
+                                 PersistentTokenRepository persistentTokenRepository) {
+        this.userDetailsService = userDetailsService;
+        this.bcryptEncoder = passwordEncoder;
+        this.jdbcTokenRepository = persistentTokenRepository;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+        auth.userDetailsService(userDetailsService).passwordEncoder(bcryptEncoder);
     }
 
     @Override
@@ -38,7 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin()
 
                 .and()
-                .rememberMe().tokenRepository(new JdbcTokenRepositoryImpl());
+                .rememberMe().tokenRepository(jdbcTokenRepository);
     }
 
     @Profile("dev")
@@ -48,8 +55,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/h2-console/**");
     }
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }

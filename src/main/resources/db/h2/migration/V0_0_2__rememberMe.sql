ae550043c65580461ffb59346cdbe577db7105f6
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2020-05-23 11:02:03 +0200

    Fixed rememberMe.sql migration

--- src/main/resources/db/h2/migration/V0_0_2__rememberMe.sql
+++ src/main/resources/db/h2/migration/V0_0_2__rememberMe.sql
@@ -7,7 +7,8 @@
 CREATE TABLE persistent_logins
 (
     username  VARCHAR(64) NOT NULL,
-    series    VARCHAR(64) PRIMARY KEY,
+    series    VARCHAR(64) NOT NULL,
     token     VARCHAR(64) NOT NULL,
-    last_used TIMESTAMP   NOT NULL
+    last_used TIMESTAMP   NOT NULL,
+    CONSTRAINT persistent_logins_series PRIMARY KEY (series)
 );
